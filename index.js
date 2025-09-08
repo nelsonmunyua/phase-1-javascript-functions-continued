6a2b38777216cfd42c262de4d64ba42c56c808ff
Nelson Munyua <nelsonmunyua8@gmail.com>
2025-09-08 08:27:11 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const { act } = require("react");
+
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun('bathe my dog');
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork('work from home');
+
+function wrapAdjective(flair = '*') {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+wrapAdjective();
